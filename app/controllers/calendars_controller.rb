fb009ba3723a19523b17e5cf3c112dcf64a4f1bc
zinmarhnin818-coder <zinmarhnin818@gmail.com>
2026-07-19 20:39:33 +0900

    Revert "Merge pull request #9 from zinmarhnin818-coder/second_training"
    This reverts commit afc078caca7d8d1d47fc11c171f0e313d621c0c7, reversing
    changes made to 88fb4416a48618eebf242708393ea32e0f704abf.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -11,10 +11,11 @@ class CalendarsController < ApplicationController
     Plan.create(plan_params)
     redirect_to action: :index
   end
+
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
@@ -33,11 +34,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      wday_num = (@todays_date + x).wday 
-    if wday_num >= 7
-      wday_num = wday_num - 7
-    end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[(@todays_date + x).wday] }
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans }
       @week_days.push(days)
     end
 
